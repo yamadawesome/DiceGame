f2320deb15b3ae093d6b60cb13254f79e4184332
yamadawesome <smileslime_0130@yahoo.co.jp>
2021-07-06 18:02:21 +0900

    Initial revision

---
+++ dicegame.java
@@ -0,0 +1,31 @@
+public class dicegame{
+    public static void main(String[] args){
+	Dice d1 = new Dice();
+	Dice d2 = new Dice();
+
+
+	System.out.println("Die 1:" + d1.getNumber());
+	System.out.println("Die 2:" + d2.getNumber());
+
+	int sum = d1.getNumber() + d2.getNumber();
+
+	System.out.println("Total value:" + sum);
+
+    }
+}
+
+
+class Dice{
+    private int num;
+    public Dice(){
+	this.roll();
+    }
+
+    public void roll(){
+	num = (int)(Math.random() * 6) + 1;
+    }
+
+    public int getNumber(){
+	return num;
+    }
+}
